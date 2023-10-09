5eadf6ad5b3e663a7b045ae38613356242909b54
Flávia Maria Cunha do Nascimento <20221170150030@ifrn.local>
2023-10-09 18:15:28 -0300

    Parte 2

--- Jogo Da Bolinha-1/Assets/Scripts/Player.cs
+++ Jogo Da Bolinha-1/Assets/Scripts/Player.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int velociade = 10;
+    public int forcaPulo = 7;
+    public bool noChao;
+    
     private Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("START");
         TryGetComponent(out rb);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChao && collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -23,6 +38,12 @@ public class Player : MonoBehaviour
 
         UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao * velociade * Time.deltaTime, ForceMode.Impulse);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
 
         if (transform.position.y <= -10)
         {
